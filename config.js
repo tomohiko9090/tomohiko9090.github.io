522fde47f11525884ada71abbc132fa28dfb8b7d
葛葉 朋彦 <tomohiko-kuzuba@M2207-3781.local>
2022-10-01 14:55:07 +0900

    files modify

---
+++ config.js
@@ -0,0 +1,12 @@
+let icon_url = 'images/IMG_7449.PNG';
+
+let user_name = '葛葉朋彦';
+
+let default_custom_message = 'テスト';
+
+let target_list = [
+// channel,    thread_ts,           API Token
+  ['#attendance', '1626826756.000900', 'xoxb-518476454566-4044688281282-TAbj9xOqLNRSJXEdFtw5ou9x'],
+  ['#kintai1', '1234567890.012300', 'xoxb-111111111111-11111111111111-aaaaaaaaaaaaaaaaaaaaaaaa'],
+  ['#kintai2', '1234567890.012300', 'xoxb-222222222222-22222222222222-bbbbbbbbbbbbbbbbbbbbbbbb'],
+];
